ab1ffef68fe05d0af0bbbb50f13d9f5e847fe1f2
tianbing <tbbd426@163.com>
2018-01-14 16:16:21 +0800

    add generator config

---
+++ model/src/main/java/com/github/trepang/ThingType.java
@@ -0,0 +1,15 @@
+package com.github.trepang;
+
+/**
+ * Created by tianbing on 2018/1/14.
+ */
+public enum ThingType {
+
+    FOOD("吃了美味");
+
+    private String desc;
+
+    ThingType(String desc) {
+        this.desc = desc;
+    }
+}
